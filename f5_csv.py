830812668d2d028e0a460e7087b54913e0a76cce
Kehinde Kamson <34457686+kila-kam@users.noreply.github.com>
2024-02-21 11:01:49 +0000

    Create f5_csv.py

---
+++ f5_csv.py
@@ -0,0 +1,26 @@
+import json
+import csv
+
+# Opening JSON file
+f = open('MyFile.txt')
+ 
+# returns JSON object as 
+# a dictionary
+data = json.load(f)
+print(data)
+# Closing file
+f.close()
+
+f = csv.writer(open("data.csv", "w"))
+f.writerow(["name", "destination", "rules", "pool", "SNAT" , "enabled"])
+for x in data['items']:
+     try:
+         f.writerow([x["name"],
+         x["destination"],
+         x.get("pool","Not available"),
+         x.get("rules","Not available"),
+         x["sourceAddressTranslation"],
+         x["enabled"]
+         ])
+     except KeyError:
+          pass
