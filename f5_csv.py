86a5e8062cc71ac42cf1b4b0b7d3054c0582398f
Kehinde Kamson <34457686+kila-kam@users.noreply.github.com>
2024-02-26 10:53:16 +0000

    Update f5_csv.py

--- f5_csv.py
+++ f5_csv.py
@@ -7,13 +7,14 @@ f = open('data.json')
 # returns JSON object as 
 # a dictionary
 data = json.load(f)
-print(data)
+#print(data)
 # Closing file
 f.close()
 
 f = csv.writer(open("data.csv", "w"))
 f.writerow(["name", "destination", "rules", "pool", "SNAT" , "enabled"])
 for x in data['items']:
+     print(x)
      try:
          f.writerow([x["name"],
          x["destination"],
